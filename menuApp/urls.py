3614aefaa300249b7941cea07be305c4fb1a5eb4
voidCaloneian <void4function@gmail.com>
2023-03-07 16:22:44 +0500

    fixes

--- menuApp/urls.py
+++ menuApp/urls.py
@@ -1,9 +1,11 @@
 #  Django модули
+from django.shortcuts import redirect
 from django.urls import path
 #  Модули проекта
 from .views import * 
 
 
 urlpatterns = [
-    path(r'<str:name>/', index),
+    path(r'<str:name>/', index, name='index'),
+    path('', lambda request: redirect('/22/')),
 ]
